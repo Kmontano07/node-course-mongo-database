a5e0410e8b6f3d38b0596f606502bfe0d9300065
Kevin Montano <kmont5@uis.edu>
2018-07-03 21:26:27 -0500

    Andrew's version of the server

--- server/server.js
+++ server/server.js
@@ -49,17 +49,16 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-app.delete('todos/:id', (req, res) => {
+
+app.delete('/todos/:id', (req, res) => {
   var id = req.params.id;
 
-  if(!ObjectID.isValid(id))
-  {
+  if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
 
   Todo.findByIdAndRemove(id).then((todo) => {
-    if(!todo)
-    {
+    if (!todo) {
       return res.status(404).send();
     }
 
