b554421a150678eef994a074814971ff5d2b3cf1
Kevin Montano <kmont5@uis.edu>
2018-07-03 21:34:27 -0500

    Final mongoose-remove

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,17 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+
+Todo.findOneAndRemove({_id:"5b3c25a95380e461fec5201b"}).then((todo) => {
+
+});
+
+Todo.findByIdAndRemove("5b3c25a95380e461fec5201b").then((todo) => {
+  console.log(todo);
+});
